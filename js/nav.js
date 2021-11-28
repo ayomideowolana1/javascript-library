2cf07eca63975327d4b6bb3b34b0547d5d3a68bd
Ayomide Owolana <ayomideowolana1@gmail.com>
2021-11-28 11:33:19 +0100

    CRUD logic implemented

---
+++ js/nav.js
@@ -0,0 +1,29 @@
+const formCont = document.querySelector("#formCont")
+const tableCont = document.querySelector("#tableCont")
+const formBtn = document.querySelector(".showForm")
+const tableBtn = document.querySelector(".showTable")
+
+function hide(x){
+    x.style.display = "none"
+}
+function show (x){
+    x.style.display = "block"
+}
+
+
+formBtn.addEventListener("click",()=>{
+    show(formCont);
+    hide(tableCont);
+})
+
+tableBtn.addEventListener("click",()=>{
+    show(tableCont);
+    hide(formCont);
+})
+
+
+clearInputs = () =>{
+    inputsArray.forEach(input =>{
+        input.value = ""
+    })
+}
